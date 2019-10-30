42bde375149654a9f2e902ce182b6a30426f22d8
Junhua <junhua.deng@ximalaya.com>
2019-10-30 15:00:46 +0800

    update

--- algo-study/src/main/java/com/junhua/algorithm/leetcode/datastructure/string/LongestPalindromicSubstring.java
+++ algo-study/src/main/java/com/junhua/algorithm/leetcode/datastructure/string/LongestPalindromicSubstring.java
@@ -10,26 +10,21 @@ public class LongestPalindromicSubstring {
      * @return
      */
     static public String longestPalindrome(String s) {
-
         if (s == null || s.length() < 2) return s;
-
         //添加分隔符
         StringBuffer sb = new StringBuffer();
         for (char c : s.toCharArray()) {
-            sb.append(1);
+            sb.append('#');
             sb.append(c);
         }
-        sb.append(1);
-
+        sb.append('#');
         int index = 0;
         int left = index - 1;
         int right = index + 1;
         int cnt = 1;
-
+        int max = 0;
         int[] count = new int[sb.length()];
-
         while (index < sb.length()) {
-
             while (left >= 0 && right < sb.length()) {
                 if (sb.charAt(left) == sb.charAt(right)) {
                     left--;
@@ -40,19 +35,58 @@ public class LongestPalindromicSubstring {
                 }
             }
             count[index++] = cnt;
+            max = Math.max(cnt, max);
             cnt = 1;
             left = index - 1;
             right = index + 1;
         }
-
-        return "";
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == max) {
+                int iLeft = max / 2;
+                int sbIndex = 0;
+                for (int j = i - iLeft; j < i + iLeft; j++) {
+                    if (sb.charAt(j) != '#') {
+                        sb.setCharAt(sbIndex++, sb.charAt(j));
+                    }
+                }
+                break;
+            }
+        }
+        return sb.substring(0, max / 2);
     }
 
 
-    public static void main(String[] args) {
-        String str = "babad";
+    private static int lo, maxLen;
 
-        longestPalindrome(str);
+    public static String longestPalindrome2(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len - 1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i + 1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    static private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "bb";
+
+        String res = longestPalindrome(str);
+
+        return;
     }
 
 }
